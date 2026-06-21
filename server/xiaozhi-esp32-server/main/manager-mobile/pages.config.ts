f7bc850063b7bc0edbc9ae63204b990b50fcd2df
zhuguang-ZFG <168892777+zhuguang-ZFG@users.noreply.github.com>
2026-06-21 22:56:51 +0800

    perf(manager-mobile): whitelist wot-design-uni components

--- server/xiaozhi-esp32-server/main/manager-mobile/pages.config.ts
+++ server/xiaozhi-esp32-server/main/manager-mobile/pages.config.ts
@@ -1,6 +1,26 @@
 import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
 import { tabBar } from './src/layouts/fg-tabbar/tabbarList'
 
+const usedWotComponents = [
+  'config-provider',
+  'toast',
+  'message-box',
+  'icon',
+  'tabbar',
+  'tabbar-item',
+  'navbar',
+  'button',
+  'action-sheet',
+  'loading',
+  'swipe-action',
+  'fab',
+  'popup',
+  'input',
+  'tag',
+  'text',
+  'status-tip',
+].join('|')
+
 export default defineUniPages({
   globalStyle: {
     navigationStyle: 'default',
@@ -13,7 +33,7 @@ export default defineUniPages({
     autoscan: true,
     custom: {
       '^fg-(.*)': '@/components/fg-$1/fg-$1.vue',
-      '^wd-(.*)': 'wot-design-uni/components/wd-$1/wd-$1.vue',
+      [`^wd-(${usedWotComponents})$`]: 'wot-design-uni/components/wd-$1/wd-$1.vue',
     },
   },
   // tabbar 的配置统一在 "./src/layouts/fg-tabbar/tabbarList.ts" 文件中
