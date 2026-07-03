7b9c821f669ec1f923ada82672d335be6b63d301
zhuguang-ZFG <168892777+zhuguang-ZFG@users.noreply.github.com>
2026-07-03 18:24:52 +0800

    chore: bump version to 3.8.2 (M1 security fixes uploaded)

--- server/xiaozhi-esp32-server/main/manager-mobile/manifest.config.ts
+++ server/xiaozhi-esp32-server/main/manager-mobile/manifest.config.ts
@@ -24,8 +24,8 @@ export default defineManifestConfig({
   'name': VITE_APP_TITLE,
   'appid': VITE_UNI_APPID,
   'description': '',
-  'versionName': '3.8.1',
-  'versionCode': '381',
+  'versionName': '3.8.2',
+  'versionCode': '382',
   'transformPx': false,
   'locale': VITE_FALLBACK_LOCALE, // 'zh-Hans'
   'h5': {
